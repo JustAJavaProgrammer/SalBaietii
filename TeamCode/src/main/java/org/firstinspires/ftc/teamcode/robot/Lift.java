0aaebd02288fc3a5197005d75d3f92c5d3e14fd4
alexandrupila <alexandrupila@yahoo.com>
2019-01-27 16:14:38 +0200

    Update Lift.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Lift.java
@@ -40,23 +40,32 @@ public class Lift {
            if (x && !robot.limitSwitchSus.getState()) {
                x = gamepad.x;
                robot.motorLift.setPower(-0.5);
-
+               robot.telemetry.addData("Liftul:","Se ridica");
+               robot.telemetry.update();
            }
            else if (y && !robot.limitSwitchJos.getState())
            {
                y=gamepad.y;
                robot.motorLift.setPower(0.5);
+               robot.telemetry.addData("Liftul:","Coboara");
+               robot.telemetry.update();
+
            }
            else if(!x || robot.limitSwitchSus.getState()) {
 
 
                robot.motorLift.setPower(0);
+               robot.telemetry.addData("Liftul:"."E oprit");
+               robot.telemetry.update();
            }else if(!y || robot.limitSwitchJos.getState())
            {
                robot.motorLift.setPower(0);
+               robot.telemetry.addData("Liftul:","E oprit");
+               robot.telemetry.update();
            } else
            {
-               robot.telemetry.addData("Lift:","Bogdan Brox e naspa.");
+               robot.telemetry.addData("Lift:","E probabil stricat");
+               robot.telemetry.update();
            }
 
 
